a38cd9b5f0bd97a4088c0610f5f74cbb26e86ed5
Natalia Cervantes <natalia.gonzalez@alumnos.udg.mx>
2020-10-11 22:34:30 -0500

    re direccion a inicio

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('catalogo','CatalogoController@index');
 Route::get('eventos','EventosController@index');
